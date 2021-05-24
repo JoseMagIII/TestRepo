ea67d1a6e8fda4bfe43ffd57b13cfda26a800782
JoseMagIII <jose_maglaqueiii@dlsu.edu.ph>
2021-05-24 18:07:09 +0800

    asda

--- controllers/AccSettController.js
+++ controllers/AccSettController.js
@@ -1,6 +1,6 @@
 
 const db = require('../models/db.js');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const saltRounds = 10;
 
 
--- controllers/RegLogController.js
+++ controllers/RegLogController.js
@@ -1,6 +1,6 @@
 const db = require('../models/db.js');
 var ObjectId = require('mongodb').ObjectID;
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const saltRounds = 10;
 const { validationResult } = require('express-validator');
 
